6b8ff9a380ef09ede6588b88cf7396fa294a91b5
Kris Wilk <kris@reefnet.ca>
2026-02-02 16:03:31 -0500

    Niri keybinds, swww to swaybg, add/refactor scripts

---
+++ user/_modules/scripts/menu-symbols-math.sh
@@ -0,0 +1,7 @@
+#!/bin/env bash
+set -euo pipefail
+
+list="$(cat "$XDG_DATA_HOME/flake-assets/symbols/math.txt")"
+dmenu="fuzzel --dmenu"
+# WIP: once fuzzel fixes "nth-delimiter" debug output bug, this can be simplified!
+echo -e "$list" | $dmenu | cut -d ' ' -f 1 | tr -d '\n' | wl-copy
