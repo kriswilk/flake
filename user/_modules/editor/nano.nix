aafe859bf691b4507efb912dc8dee6789d262785
Kris Wilk <kris@reefnet.ca>
2026-02-06 10:32:16 -0500

    nano: configure options

--- user/_modules/editor/nano.nix
+++ user/_modules/editor/nano.nix
@@ -4,4 +4,32 @@
   home.packages = with pkgs; [
     nano
   ];
+
+  xdg.configFile."nano/nanorc".text = ''
+    set atblanks
+    set autoindent
+    set colonparsing
+    set constantshow
+
+    set indicator
+    set scrollercolor white,lightblack
+
+    set keycolor lightwhite,black
+
+    set linenumbers
+    set numbercolor italic,lightwhite,black
+
+    set minibar
+    set minicolor black,blue
+
+    set mouse
+    set nohelp
+
+    set promptcolor black,green
+
+    set smarthome
+    set softwrap
+    set tabsize 4
+    set tabstospaces
+  '';
 }
