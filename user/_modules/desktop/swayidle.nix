b390ba77b5b10d1237f8c7137690540db2231474
Kris Wilk <kris@reefnet.ca>
2026-02-08 00:48:29 -0500

    swayidle: set timeout to lock (5 min)

--- user/_modules/desktop/swayidle.nix
+++ user/_modules/desktop/swayidle.nix
@@ -1,10 +1,11 @@
 { config, lib, pkgs, ... }:
 
 {
+  # WIP: determine best timeouts, proper commands
   services.swayidle = {
     enable = true;
     timeouts = [
-      #{ timeout = 10; command = "${pkgs.swaylock}/bin/swaylock -f"; }
+      { timeout = 300; command = "${pkgs.swaylock}/bin/swaylock -f"; }
       #{ timeout = 90; command = "systemctl suspend"; }
     ];
     events = {
