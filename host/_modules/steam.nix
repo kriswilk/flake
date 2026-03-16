f691ac7f29a2fe850df605e0b09d9b3d3be9d0d0
Kris Wilk <kris@reefnet.ca>
2026-03-15 21:39:06 -0400

    steam: fix for black screen (desktop UI)

--- host/_modules/steam.nix
+++ host/_modules/steam.nix
@@ -5,6 +5,12 @@
 {
   programs.steam = {
     enable = true;
+
+    # fixes black screen on steam UI (web views)
+    package = pkgs.steam.override {
+      extraArgs = "-system-composer";
+    };
+
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
     localNetworkGameTransfers.openFirewall = true;
